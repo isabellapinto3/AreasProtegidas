cc19f4c00e7707a3a78fa24c92c7b01b956ddcb4
IvanLuciano <ivan09449@gmail.com>
2021-11-11 17:36:26 -0300

    a

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
- 
+$(document).ready(function(){
+    $("#botonIniciar").click(function() {
+      $("#pregunta-1").show();
+      $("#pregunta-2").hide();
+      $("#pregunta-3").hide();
+      $("#pregunta-3-2").hide();
+      $("#pregunta-4").hide();
+      //
+      $(".portada-encuesta").hide();
+
+    });
+});
