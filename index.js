24d1ae8bd7ba49cac71fa7d071e5c0fa6bf8c534
Mariano Quattrocchi <marianoq_91@hotmail.com>
2021-11-11 21:41:41 -0300

    .

--- index.js
+++ index.js
@@ -1,147 +1,164 @@
-$(document).ready(function(){
+$(document).ready(function () {
+  //
+  // CAMBIOS DE PANTALLA
+  //
+  $("#botonIniciar").click(function () {
+    $("#pregunta-1").show();
+    $("#pregunta-2").hide();
+    $("#pregunta-3").hide();
+    $("#pregunta-3-2").hide();
+    $("#pregunta-4").hide();
     //
-    // CAMBIOS DE PANTALLA
-    //
-    $("#botonIniciar").click(function() {
-      $("#pregunta-1").show();
-      $("#pregunta-2").hide();
-      $("#pregunta-3").hide();
-      $("#pregunta-3-2").hide();
-      $("#pregunta-4").hide();
-      //
-      $(".portada-encuesta").hide();
-    });
+    $(".portada-encuesta").hide();
+  });
 
-    $(".preg1-img").click(function() {
-      $("#pregunta-1").hide();
-      $("#pregunta-2").show();
-      $("#pregunta-3").hide();
-      $("#pregunta-3-2").hide();
-      $("#pregunta-4").hide();
-    });
+  $(".preg1-img").click(function () {
+    $("#pregunta-1").hide();
+    $("#pregunta-2").show();
+    $("#pregunta-3").hide();
+    $("#pregunta-3-2").hide();
+    $("#pregunta-4").hide();
+  });
 
-    $("#p2b1").click(function() {
-      $("#pregunta-1").hide();
-      $("#pregunta-2").hide();
-      $("#pregunta-3").show();
-      $("#pregunta-3-2").hide();
-      $("#pregunta-4").hide();
-    });
+  $("#p2b1").click(function () {
+    $("#pregunta-1").hide();
+    $("#pregunta-2").hide();
+    $("#pregunta-3").show();
+    $("#pregunta-3-2").hide();
+    $("#pregunta-4").hide();
+  });
 
-    $("#p2b2").click(function() {
-      $("#pregunta-1").hide();
-      $("#pregunta-2").hide();
-      $("#pregunta-3").hide();
-      $("#pregunta-3-2").show();
-      $("#pregunta-4").hide();
-    });
+  $("#p2b2").click(function () {
+    $("#pregunta-1").hide();
+    $("#pregunta-2").hide();
+    $("#pregunta-3").hide();
+    $("#pregunta-3-2").show();
+    $("#pregunta-4").hide();
+  });
 
-    $(".preg3-img").click(function() {
-      $("#pregunta-1").hide();
-      $("#pregunta-2").hide();
-      $("#pregunta-3").hide();
-      $("#pregunta-3-2").hide();
-      $("#pregunta-4").show();
-    });
-    $(".preg32-img").click(function() {
-      $("#pregunta-1").hide();
-      $("#pregunta-2").hide();
-      $("#pregunta-3").hide();
-      $("#pregunta-3-2").hide();
-      $("#pregunta-4").show();
-    });
-    //
-    // VARIABLES
-    //
-    // preg 1
-    var region, visitante, estacion, especie, hospedaje;
-    $("#p1b1,#p1b3").click(function() {
+  $(".preg3-img").click(function () {
+    $("#pregunta-1").hide();
+    $("#pregunta-2").hide();
+    $("#pregunta-3").hide();
+    $("#pregunta-3-2").hide();
+    $("#pregunta-4").show();
+  });
+  $(".preg32-img").click(function () {
+    $("#pregunta-1").hide();
+    $("#pregunta-2").hide();
+    $("#pregunta-3").hide();
+    $("#pregunta-3-2").hide();
+    $("#pregunta-4").show();
+  });
+  //
+  // VARIABLES
+  //
+  // preg 1
+  let region, visitante, estacion, especie, hospedaje;
+  $("#p1b1,#p1b3").click(function () {
     region = "calor";
     console.log(region);
-    });
-    $("#p1b4,#p1b2").click(function() {
+  });
+  $("#p1b4,#p1b2").click(function () {
     region = "frio";
     console.log(region);
-    });
-    // preg 2
-    $("#p2b1").click(function() {
+  });
+  // preg 2
+  $("#p2b1").click(function () {
     visitante = "activo";
     console.log(visitante);
-    });
-    $("#p2b2").click(function() {
+  });
+  $("#p2b2").click(function () {
     visitante = "pasivo";
     console.log(visitante);
-    });
-    // preg 3
-    $("#p3b1,#p3b3").click(function() {
+  });
+  // preg 3
+  $("#p3b1,#p3b3").click(function () {
     estacion = "calor";
     console.log(estacion);
-    });
-    $("#p3b4,#p3b2").click(function() {
+  });
+  $("#p3b4,#p3b2").click(function () {
     estacion = "frio";
     console.log(estacion);
-    });
-    // preg 3-2
-    $("#p32b1,#p32b3").click(function() {
+  });
+  // preg 3-2
+  $("#p32b1,#p32b3").click(function () {
     especie = "flora";
     console.log(especie);
-    });
-    $("#p32b4,#p32b2").click(function() {
+  });
+  $("#p32b4,#p32b2").click(function () {
     especie = "fauna";
     console.log(especie);
-    });
-    // preg 4
-    $("#p4b1").click(function() {
+  });
+  // preg 4
+  $("#p4b1").click(function () {
     hospedaje = "carpa";
     console.log(hospedaje);
-    });
-    $("#p4b2").click(function() {
+    selector();
+  });
+  $("#p4b2").click(function () {
     hospedaje = "hotel";
     console.log(hospedaje);
-    });
+    selector();
+  });
 
+  function selector() {
     //  Condicionales encuesta
     //frio
     //Nahuel huapi
-    if(region == "frio" && visitante == "activo" && estacion == "frio"){
-      //var x = document.getElementById('nahuelhuapi.html').innerHTML;   
-      console.log("hola");   
+    if (region == "frio" && visitante == "activo" && estacion == "frio") {
+      
+      $(".preg4-img").click(function() {
+        window.location.replace('nahuelhuapi.html');
+        return false;
+     });
+      //var x = document.getElementById('nahuelhuapi.html').innerHTML;
+      console.log("hola");
     }
     //Los glaciares
-    if(region == "frio" && visitante == "activo" && estacion == "calor" && hospedaje =="hotel"){
+    if (region == "frio" && visitante == "activo" && estacion == "calor" && hospedaje == "hotel") {
       var x = document.getElementById('nahuelhuapi.html').innerHTML;
     }
     //Tierra del fuego
-    if(region == "frio" && visitante == "activo" && estacion == "calor" && hospedaje =="carpa"){
+    if (region == "frio" && visitante == "activo" && estacion == "calor" && hospedaje == "carpa") {
       var x = document.getElementById('nahuelhuapi.html').innerHTML;
     }
     //Lago puelo
-    if(region == "frio" && visitante == "pasivo" && especie == "flora"){
+    if (region == "frio" && visitante == "pasivo" && especie == "flora") {
       var x = document.getElementById('nahuelhuapi.html').innerHTML;
     }
     //Arrayanes
-    if(region == "frio" && visitante == "pasivo" && especie == "fauna"){
+    if (region == "frio" && visitante == "pasivo" && especie == "fauna") {
       var x = document.getElementById('nahuelhuapi.html').innerHTML;
     }
 
-    
+
     //calor
     //Iguazu
-    if(region == "calor" && visitante == "activo" && estacion == "calor" && hospedaje =="hotel"){
+    if (region == "calor" && visitante == "activo" && estacion == "calor" && hospedaje == "hotel") {
       var x = document.getElementById('nahuelhuapi.html').innerHTML;
     }
     //El palmar
-    if(region == "calor" && visitante == "activo" && estacion == "calor" && hospedaje =="carpa"){
+    if (region == "calor" && visitante == "activo" && estacion == "calor" && hospedaje == "carpa") {
       console.log(hola);
       var x = document.getElementById('nahuelhuapi.html').innerHTML;
     }
     //Los cardones
-    if(region == "calor" && visitante == "pasivo" && especie == "fauna"){
+    if (region == "calor" && visitante == "pasivo" && especie == "fauna") {
       var x = document.getElementById('nahuelhuapi.html').innerHTML;
     }
     //Talampaya
-    if(region == "calor" && visitante == "pasivo" && especie == "flora"){
+    if (region == "calor" && visitante == "pasivo" && especie == "flora") {
       var x = document.getElementById('nahuelhuapi.html').innerHTML;
     }
+  }
 
+
+
+
+});
+
+$('.open-link').click(function(){
+  window.open('nahuelhuapi.html');
+  return false;
 });
